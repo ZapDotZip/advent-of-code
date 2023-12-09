bd5d025438797324dc1b3b3a62aaa6e5ef20a2b2
zap.zip <zapdotzip@gmail.com>
2023-12-09 12:40:39 -0700

    reorganize repo

--- 2023/01/01.c
+++ 2023/01/01.c
@@ -22,6 +22,24 @@ user	0m0.175s
 */
 
 
+
+
+// one, two, three, four, five, six, seven, eight, nine
+const char* one = "one";
+const char* two = "two";
+const char* three = "three";
+const char* four = "four";
+const char* five = "five";
+const char* six = "six";
+const char* seven = "seven";
+const char* eight = "eight";
+const char* nine = "nine";
+
+char wordcheck(char c) {
+	
+}
+
+
 int main(int argc, char *argv[]) {
 	// load file
 	if(argc != 2) { exit(0); }
@@ -34,11 +52,11 @@ int main(int argc, char *argv[]) {
 	
 	short f = 0;
 	short l = 0;
-	char first = 0;
-	char last = 1;
-	//char c = '\n';
+	char first = '\0';
+	char last = '\0';
 	int total = 0;
 	for(int i = 0; i < st.st_size; i++) {
+		// end of line, convert the found numbers to an int
 		if(fptr[i] == '\n') {
 			char a[] = {first, last, '\0'};
 			total += atoi(a);
@@ -47,31 +65,40 @@ int main(int argc, char *argv[]) {
 			first = '\0';
 			last = '\0';
 		}
+		// check if each char is a digit
 		if(isdigit(fptr[i])) {
+			// if the first num hasn't been found, set it as the current char
 			if(!f) {
 				first = fptr[i];
 				f = 1;
 				l = 0;
 			}
+			// if the last char hasn't been found, set it as the current char
 			if(!l) {
 				last = fptr[i];
 				l = 0;
 			}
+		} else {
+			// check the next 5? chars to see if they make up a number word
+			char c = wordcheck(fptr[i]);
+			// it returns the char of the number, or \0 if nan
+			if(c != '\0') {
+				if(!f) {
+					first = c;
+					f = 1;
+					l = 0;
+				}
+				if(!l) {
+					last = c;
+					l = 0;
+				}
+			}
 		}
 	}
 	char a[] = {first, last, '\0'};
+	printf("FINAL STR: %c%c\n", first, last);
 	total += atoi(a);
 	close(fd);
 	printf("total: %d\n", total);
 }
 
-// one, two, three, four, five, six, seven, eight, nine
-// largest is 5 chars
-// so need char word[5]
-// to compare against wordlist
-
-
-// store each word, compare against each
-// vs
-// compare each letter and have a 0/1
-// aka inline compare?
