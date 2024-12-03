202402c3fe0f77eed5159dd667200fae87b2f468
zap.zip <zapdotzip@gmail.com>
2024-12-02 20:58:35 -0700

    day 2: part 2 finished

---
+++ 2024/02/part2.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+let data = try! String(contentsOfFile: CommandLine.arguments[1])
+let lines: [[Int]] = data.split(separator: "\n").map { (str) -> [Int] in
+	return str.split(separator: " ").map { (slice) -> Int in
+		Int(slice)!
+	}
+}
+
+/// Returns true if it's unsafe
+func check(_ a: Int, _ b: Int, _ isIncreasing: Bool) -> Bool {
+	let diff = abs(a - b)
+	return diff < 1 || diff > 3 || (a - b > 0) == isIncreasing
+}
+
+/// Returns true if it's unsafe
+func isSafe(_ arr: [Int]) -> Bool {
+	var last = arr.first!
+	let isIncreasing = last - arr[1] > 0
+	for i in arr[1...arr.count-1] {
+		if check(i, last, isIncreasing) {
+			return false
+		}
+		last = i
+	}
+	return true
+}
+
+var safeCount = 0
+for line in lines {
+	if !isSafe(line) {
+		for i in 0...line.count-1 {
+			var subline = line
+			subline.remove(at: i)
+			if isSafe(subline) {
+				safeCount += 1
+				break
+			}
+		}
+	} else {
+		safeCount += 1
+	}
+}
+
+print(safeCount)
