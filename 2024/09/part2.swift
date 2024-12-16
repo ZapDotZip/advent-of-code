202409bf33feea9362d53b27538e90d94fd1fb67
zap.zip <zapdotzip@gmail.com>
2024-12-15 20:17:10 -0700

    day 9: part 2 done

---
+++ 2024/09/part2.swift
@@ -0,0 +1,70 @@
+import Foundation
+var input = (try! String(contentsOfFile: CommandLine.arguments[1])).split(separator: "\n").first!
+var blocks = [String]()
+var freeSpace = false
+var inode = 0
+for char in input {
+	if freeSpace {
+		blocks.append(contentsOf: [String].init(repeating: ".", count: Int(String(char))!))
+	} else {
+		blocks.append(contentsOf: [String].init(repeating: String(inode), count: Int(String(char))!))
+		inode += 1
+	}
+	freeSpace = !freeSpace
+}
+
+func findContiguousSize(item: String, startPos: Int) -> Int {
+	for i in stride(from: startPos, to: 0, by: -1) {
+		if blocks[i] != item { return startPos - i }
+	}
+	return -1
+}
+
+func findEnoughFreeSpace(size: Int, item: String, upTo: Int) -> Bool {
+	var freeSpace = 0
+	var freeSpaceStart = -1
+	for (index, i) in blocks.enumerated().prefix(upTo) {
+		if i == "." {
+			freeSpace += 1
+			if freeSpaceStart == -1 {
+				freeSpaceStart = index
+			}
+			if freeSpace >= size {
+				for j in freeSpaceStart...(freeSpaceStart + size - 1) {
+					blocks[j] = item
+				}
+				return true
+			}
+		} else {
+			freeSpace = 0
+			freeSpaceStart = -1
+		}
+	}
+	return false
+}
+
+
+var index = blocks.endIndex - 1
+while index > 0 {
+	let srcBlockLen = findContiguousSize(item: blocks[index], startPos: index)
+	if srcBlockLen == -1 {
+		break
+	}
+	if findEnoughFreeSpace(size: srcBlockLen, item: blocks[index], upTo: index) {
+		for j in stride(from: index, to: index-srcBlockLen, by: -1) {
+			blocks[j] = "."
+		}
+	}
+	index -= srcBlockLen
+}
+
+var result = 0
+for (index, i) in blocks.enumerated() {
+	if let id = Int(i) {
+		result += index * id
+	} else {
+		continue
+	}
+}
+// print(blocks.reduce(into: "") { $0 = "\($0)\($1)" })
+print(result)
